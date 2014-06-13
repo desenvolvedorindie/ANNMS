65ac98004d77683976d72035302e7bd340772400
wfcreations <wfcreations@gmail.com>
2014-06-13 13:27:25 -0300

    Auth fix

--- src/br/com/wfcreations/annms/core/auth/User.java
+++ src/br/com/wfcreations/annms/core/auth/User.java
@@ -1,3 +1,32 @@
+/*
+ * Copyright (c) 2013, Welsiton Ferreira (wfcreations@gmail.com)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice, this
+ *  list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice, this
+ *  list of conditions and the following disclaimer in the documentation and/or
+ *  other materials provided with the distribution.
+ *
+ *  Neither the name of the WFCreation nor the names of its
+ *  contributors may be used to endorse or promote products derived from
+ *  this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package br.com.wfcreations.annms.core.auth;
 
 import java.io.Serializable;
@@ -10,27 +39,42 @@ public class User implements Serializable {
 
 	protected String password;
 
+	protected boolean isSuper = false;
+
+	public User(String username, String password, boolean isSuper) {
+		this.setUsername(username).setPassword(password).setSuper(isSuper);
+	}
+
 	public User(String username, String password) {
-		this.username = username;
-		this.password = password;
+		this(username, password, false);
 	}
 
 	public String getUsername() {
 		return username;
 	}
 
-	public void setUsername(String username) {
+	public User setUsername(String username) {
+		if (username == null)
+			throw new IllegalArgumentException("Username can't be null");
+		if (username.equals(""))
+			throw new IllegalArgumentException("Username can't be empty");
 		this.username = username;
+		return this;
 	}
 
 	public String getPassword() {
 		return password;
 	}
 
-	public void setPassword(String password) {
+	public User setPassword(String password) {
+		if (password == null)
+			throw new IllegalArgumentException("Password can't be null");
+		if (password.equals(""))
+			throw new IllegalArgumentException("Password can't be empty");
 		this.password = password;
+		return this;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -60,6 +104,15 @@ public class User implements Serializable {
 		} else if (!username.equals(other.username))
 			return false;
 		return true;
+	}
+
+	public boolean isSuper() {
+		return isSuper;
+	}
+
+	public User setSuper(boolean isSuper) {
+		this.isSuper = isSuper;
+		return this;
 	}
 
 	@Override
